edacc8a5b6261b6de67958883dad662e1ac6f5db
EddieOrozco <orozcoe230@gmail.com>
2020-11-13 07:48:32 -0600

    Created form components and pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/HomePage.component';
 import ShopPage from './pages/shop/ShopPage.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 import Header from './components/header/header';
 
 import './App.css';
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signIn' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
